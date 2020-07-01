1035bc3333b86f0862a2e49831b3809e5264d85f
Anton Kudryavtsev <boblobl4@gmail.com>
2020-07-01 16:53:18 +0300

    wasm imports test

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,13 @@
 // The entry file of your WebAssembly module.
 
+interface Element {}
+
+export declare function createElement(text: string): void;
+
 export function add(a: i32, b: i32): i32 {
   return a + b;
+}
+
+export function hello(): void {
+  createElement("Hello World!");
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import loader from "@assemblyscript/loader";
 
 const imports = {
-  /* imports go here */
+  index: {
+    createElement(text) {
+      const el = document.createElement("h1");
+      el.textContent = text;
+      document.body.append(el);
+    },
+  },
 };
 
 async function main() {
   const { exports } = await loader.instantiate(fetch("optimized.wasm"), imports);
   console.log(exports.add(1, 2));
+  console.log(exports.hello());
 }
 
 main();
