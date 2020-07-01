8ee1fcc8712033ccc655dfc206917e58716186d0
Anton Kudryavtsev <boblobl4@gmail.com>
2020-07-01 23:14:54 +0300

    working string, but no wasm imports

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,12 +2,10 @@
 
 interface Element {}
 
-export declare function createElement(text: string): void;
-
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function hello(): void {
-  createElement("Hello World!");
+export function hello(): string {
+  return "Hello World!";
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import loader from "@assemblyscript/loader";
 
 const imports = {
-  index: {
-    createElement(text) {
-      const el = document.createElement("h1");
-      el.textContent = text;
-      document.body.append(el);
-    },
-  },
+  index: {},
 };
 
 async function main() {
   const { exports } = await loader.instantiate(fetch("optimized.wasm"), imports);
   console.log(exports.add(1, 2));
-  console.log(exports.hello());
+
+  const helloString = exports.__getString(exports.hello());
+  console.log(helloString);
+
+  const el = document.createElement("h1");
+  el.textContent = helloString;
+  document.body.append(el);
 }
 
 main();
